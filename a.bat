3e843c6b567daecb989c32fa82c4fe7ec72e2572
cmal <zyzy5730@163.com>
2012-07-21 08:50:59 +0800

    new file:   +2magicbane/pom-pom.NetHack-saved-game 	new file:   1/pom-pom.NetHack-saved-game 	new file:   2/pom-pom.NetHack-saved-game 	new file:   3/pom-pom.NetHack-saved-game 	new file:   4/pom-pom.NetHack-saved-game 	new file:   5/pom-pom.NetHack-saved-game 	new file:   6/pom-pom.NetHack-saved-game 	new file:   7-magicbane/pom-pom.NetHack-saved-game 	new file:   8+7magicbane/pom-pom.NetHack-saved-game 	new file:   Dlvl10/pom-pom.NetHack-saved-game 	new file:   Dlvl12/pom-pom.NetHack-saved-game 	new file:   Dlvl8/pom-pom.NetHack-saved-game 	new file:   a.bat 	new file:   b.bat 	modified:   pom-pom.NetHack-saved-game

---
+++ a.bat
@@ -0,0 +1,1 @@
+copy /y D:\nethack\pom-pom.NetHack-saved-game D:\nethack-saved\identify\
---
+++ b.bat
@@ -0,0 +1,3 @@
+copy /y D:\nethack\pom-pom.Nethack-saved-game D:\nethack\tmp\
+copy /y D:\nethack-saved\identify\pom-pom.Nethack-saved-game D:\nethack\
+D:\nethack\nethack.exe
